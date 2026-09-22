829109db578348760aa3ae09ae85a42e4bac8844
Harish Kumar <harishk3445@gmail.com>
2026-09-23 01:41:52 +0530

    Time: 43 ms (82.70%) | Memory: 28.9 MB (92.38%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -25,7 +25,7 @@ public:
                     while (left < right && nums[left] == nums[left - 1])
                         left++;
 
-                    while (right >= 0 && nums[right] == nums[right + 1])
+                    while (left < right && nums[right] == nums[right + 1])
                         right--;
                 } else if (sum < 0){
                     left++;
